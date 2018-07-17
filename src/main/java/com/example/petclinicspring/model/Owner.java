9250119c6e789bfa991049087d21ece4522d5f34
SlawomirR <no.for.spam@wp.pl>
2018-07-17 12:55:14 +0200

    Added POJOs. Closes #2.

---
+++ src/main/java/com/example/petclinicspring/model/Owner.java
@@ -0,0 +1,5 @@
+package com.example.petclinicspring.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/com/example/petclinicspring/model/Vet.java
@@ -0,0 +1,5 @@
+package com.example.petclinicspring.model;
+
+public class Vet extends Person {
+
+}
