e58d1d395e110fbb9f3e5ffe187478b5ab3db1e4
SlawomirR <no.for.spam@wp.pl>
2018-07-23 13:46:30 +0200

    Implemented Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/example/petclinicspring/services/OwnerService.java
@@ -0,0 +1,16 @@
+package com.example.petclinicspring.services;
+
+import com.example.petclinicspring.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/example/petclinicspring/services/VetService.java
@@ -0,0 +1,14 @@
+package com.example.petclinicspring.services;
+
+import com.example.petclinicspring.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
