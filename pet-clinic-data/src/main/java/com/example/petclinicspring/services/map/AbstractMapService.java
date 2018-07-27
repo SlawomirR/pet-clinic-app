bbb7c988211778b76c9af294e9ec060e91f46754
SlawomirR <no.for.spam@wp.pl>
2018-07-27 12:13:08 +0200

    Changed typo and access modifiers.

--- pet-clinic-data/src/main/java/com/example/petclinicspring/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/example/petclinicspring/services/map/AbstractMapService.java
@@ -5,9 +5,9 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
+abstract class AbstractMapService<T, ID> {
 
-    protected Map<ID, T> map = new HashMap<>();
+    private Map<ID, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -17,9 +17,9 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T objext) {
-        map.put(id, objext);
-        return objext;
+    T save(ID id, T object) {
+        map.put(id, object);
+        return object;
     }
 
     void deleteById(ID id) {
