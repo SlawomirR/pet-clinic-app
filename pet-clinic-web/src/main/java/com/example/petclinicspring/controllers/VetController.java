dffc1115224de79014712fcaa402ae4df09a6e14
SlawomirR <no.for.spam@wp.pl>
2018-07-30 14:37:32 +0200

    Simplifying mapping methods path for endpoints.

--- pet-clinic-web/src/main/java/com/example/petclinicspring/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/petclinicspring/controllers/VetController.java
@@ -2,11 +2,14 @@ package com.example.petclinicspring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping(method = RequestMethod.GET, path = "/vets")
 public class VetController {
 
-    @GetMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @GetMapping({"", "/index", "/index.html"})
     public String listVets() {
 
         return "vets/index";
