7bf7401bf491a938df58ae2d1875d0aedd6ae159
SlawomirR <no.for.spam@wp.pl>
2018-10-07 21:33:38 +0200

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/io/github/slawomirr/petclinicspring/services/map/PetTypeService.java
@@ -0,0 +1,7 @@
+package io.github.slawomirr.petclinicspring.services.map;
+
+import io.github.slawomirr.petclinicspring.model.PetType;
+import io.github.slawomirr.petclinicspring.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
